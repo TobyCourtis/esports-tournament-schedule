a818219902a21b653bfd1da827c6446fe49e2ead
Toby Courtis <toby.courtis@hotmail.com>
2021-09-18 10:55:57 +0100

    ✨ Altered rendering mechanism to render tournaments in a div for specific month

--- flask-server/react-frontend/src/components/TournamentsTable.tsx
+++ flask-server/react-frontend/src/components/TournamentsTable.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import {ITournament} from '../types/TournamentTypes';
 import './TournamentTable.css'
+import {Months} from "../types/Months";
+import TournamentsByMonth from "./TournamentsByMonth";
 
 interface IProps {
     tournaments: ITournament[]
@@ -8,26 +10,22 @@ interface IProps {
 
 function TournamentsTable(props: IProps) {
 
+    var monthNow = new Date().getMonth()
+    var months: string[] = []
+    for (let i = 0; i < 5; i++) {
+        months.push(Months[monthNow % 12])
+        monthNow++
+    }
+
     return (
         <div>
             {
-                props.tournaments.map((tournament) => (
-                    <div id='tournaments-div'>
-                        <div className='tournament-date-heading'>
-                            <p className='tournament-date'>{tournament.Date.toLocaleString().substring(0, 12)}</p>
-                        </div>
-                        <div className='tournament-data'>
-                            <p className='tournament-data-item'>{tournament.Name}</p>
-                            <p className='tournament-data-item'>{tournament.PrizePool}</p>
-                            <a className='tournament-data-item' href={tournament.Stream}>
-                                {tournament.Stream.split('//')[1]}
-                            </a>
-                            <a className='tournament-data-item' href={tournament.FormatLink}>
-                                {tournament.FormatName}
-                            </a>
-                        </div>
-                        <hr className='hr-solid'/>
-                    </div>))
+                months.map((month) => (
+                    <div>
+                        <h2>{month}</h2>
+                        <TournamentsByMonth monthValue={month} tournaments={props.tournaments}/>
+                    </div>
+                ))
             }
         </div>
     );
