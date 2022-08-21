f738b5cd1bfdb55725bb0c9142868ea64a513f6b
tcourtis <toby.courtis@hotmail.com>
2022-08-21 11:36:27 +0100

    ✨ Added player total to top of players page

--- src/react-frontend/src/components/players/PlayersGrid.tsx
+++ src/react-frontend/src/components/players/PlayersGrid.tsx
@@ -9,7 +9,6 @@ interface IProps {
 }
 
 
-
 function PlayersGrid({players}: IProps) {
 
     const featuredPlayers = ["Tommey", "Aydan", "Rated"]
@@ -17,6 +16,11 @@ function PlayersGrid({players}: IProps) {
     return (
         <div className={'players-table-outer-div'}>
             <div className={'players-table-inner-div'}>
+                <div className={'players-total-div'}>
+                    <p className={'players-total'}>SHOWING</p>
+                    <p className={'players-total player-length'}>{players.length}</p>
+                    <p className={'players-total'}>PLAYERS</p>
+                </div>
                 <h1 className={'headings'}>Featured</h1>
                 {/* TODO refactor into component to use twice */}
                 <Grid container spacing={3} zeroMinWidth>
